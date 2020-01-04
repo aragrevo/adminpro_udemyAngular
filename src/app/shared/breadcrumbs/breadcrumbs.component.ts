30ef6e70ea2297ef3c44e02d534fa238d02d9025
Eduardo Vergara <eduver_san@hotmail.com>
2020-01-04 18:19:48 -0500

    metaTags

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
-import { Title } from '@angular/platform-browser';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -14,12 +14,20 @@ export class BreadcrumbsComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private titleBrowser: Title) {
+    private titleBrowser: Title,
+    private meta: Meta) {
 
     this.getDataRoute()
       .subscribe(data => {
         this.title = data.title;
         this.titleBrowser.setTitle(`AdminPro: ${this.title}`);
+
+        const metaTag: MetaDefinition = {
+          name: 'description',
+          content: this.title
+        };
+
+        this.meta.updateTag(metaTag);
       });
   }
 
